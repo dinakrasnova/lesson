24ea2c27139846ff92929c3ae1f3b134755ca79b
dinakrasnova <dinakrasnova@bk.ru>
2020-01-08 09:42:20 +0500

    les3

--- lesson3.py
+++ lesson3.py
@@ -1,10 +1,16 @@
 #1
 import re
-valids = re.sub(r"[^A-Za-zА-Яа-я]+", '', 'Hello,!?')
-print(valids)
+import string
+from typing import Any
+
+text = open('text.txt', 'r', encoding = 'utf-8')
+text = text.read()
+
+text = (re.sub("[" + re.escape(string.punctuation) + "]", "", text))
+print(text)
 
 #2
-list_temp = ['Orange', 'apple', 'Grape', 'orange', 'Cherry']
+list_temp = text
 print(list_temp)
 
 #3
@@ -13,18 +19,19 @@ print(list_temp)
 
 #4
 
-key_lst = ['orange', 'apple', 'grape', 'orange', 'cherry']
-value_lst = [2, 1, 1, 2, 1]
-my_dict = dict(zip(key_lst, value_lst))
-print(my_dict)
+list_dict = {}
+for i in range(len(list_temp)):
+    list_dict[list_temp[i]] = list_temp.count(list_temp[i])
+import operator
+x = list_dict
+list_dict = sorted(x.items(), key=operator.itemgetter(1), reverse = True)
 
 #5
 
-my_list = ['orange', 'apple', 'grape', 'orange', 'cherry', 'apple', 'melon', 'grape', 'cherry']
-my_list.sort(reverse = True)
-print(my_list)
+for i in range(5):
+    print(list_dict[i])
 
-my_set = set(my_list)
+my_set = set(list_temp)
 print(my_set)
 
 
